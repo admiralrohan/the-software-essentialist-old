3016b7580089f6337ca6991907be52b5064da92d
Rohan Gayen <rohangayen@gmail.com>
2023-03-16 13:26:05 +0530

    red: list of test cases

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,10 @@
+import fizzbuzz from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-
-
+  it("should accept from 1 to 100", () => {
+    expect(fizzbuzz(3, 15)).toBeTruthy();
+  });
+  it("should not accept numbers less than 1", () => {});
+  it("should not accept numbers greater than 100", () => {});
+  it("should return numbers as strings", () => {});
 });
