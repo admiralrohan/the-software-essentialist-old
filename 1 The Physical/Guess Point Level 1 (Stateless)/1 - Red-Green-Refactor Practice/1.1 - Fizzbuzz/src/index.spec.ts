0b4407c3950ef171fc2c787ed78e6fef0bdf7294
Rohan Gayen <rohangayen@gmail.com>
2023-03-16 13:44:05 +0530

    red: should not accept numbers less than 1

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
@@ -4,7 +4,9 @@ describe("fizzbuzz", () => {
   it("should accept from 1 to 100", () => {
     expect(fizzbuzz([3, 5, 7, 15])).toBeTruthy();
   });
-  it("should not accept numbers less than 1", () => {});
+  it("should not accept numbers less than 1", () => {
+    expect(() => fizzbuzz([-1])).toThrow();
+  });
   it("should not accept numbers greater than 100", () => {});
   it("should return numbers as strings", () => {});
 });
