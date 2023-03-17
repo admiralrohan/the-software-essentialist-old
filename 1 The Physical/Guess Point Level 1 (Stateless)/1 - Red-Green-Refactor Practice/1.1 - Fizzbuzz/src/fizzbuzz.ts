8a67aca41ee2731e8a21dd01d3dab1ad7cfdbfa4
Rohan Gayen <rohangayen@gmail.com>
2023-03-17 15:04:16 +0530

    green: passing test

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/fizzbuzz.ts
@@ -3,10 +3,15 @@ function fizzbuzz(inputs: number[]) {
     if (currVal < 1) throw new Error("Less that 1 is not allowed");
     if (currVal > 100) throw new Error("More that 100 is not allowed");
 
-    return acc + currVal.toString();
+    let valueToAdd = currVal.toString();
+    if (currVal % 3 === 0) valueToAdd = "Fizz";
+    if (currVal % 5 === 0) valueToAdd = "Buzz";
+    if (currVal % 3 === 0 && currVal % 5 === 0) valueToAdd = "FizzBuzz";
+
+    return acc + valueToAdd;
   }, "");
 
-  return "hi";
+  return result;
 }
 
 export default fizzbuzz;
