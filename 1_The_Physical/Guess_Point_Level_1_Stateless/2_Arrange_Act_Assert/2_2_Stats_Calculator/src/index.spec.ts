6df93c5b6ee14bc3b8a417d3e9e542f308361f7c
Rohan Gayen <rohangayen@gmail.com>
2023-03-19 18:08:30 +0530

    solution

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,23 @@
+import statsCalculator from "./index";
 
-describe('stats calculator', () => {
+describe("stats calculator", () => {
+  it("should return minimum value", () => {
+    const result = statsCalculator([2, 4, 21, -8, 53, 40]);
+    expect(result.min).toBe(-8);
+  });
 
+  it("should return maximum value", () => {
+    const result = statsCalculator([2, 4, 21, -8, 53, 40]);
+    expect(result.max).toBe(53);
+  });
 
-})
+  it("should return no of elements in sequence", () => {
+    const result = statsCalculator([2, 4, 21, -8, 53, 40]);
+    expect(result.noOfElements).toBe(6);
+  });
+
+  it("should return average value", () => {
+    const result = statsCalculator([2, 4, 21, -8, 53, 40]);
+    expect(result.avg).toBe(18.666666666667);
+  });
+});
