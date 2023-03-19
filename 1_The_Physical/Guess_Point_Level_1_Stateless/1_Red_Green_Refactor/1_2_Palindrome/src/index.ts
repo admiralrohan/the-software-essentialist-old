a42778c0935dc04051c4723bff883a714c6629de
Rohan Gayen <rohangayen@gmail.com>
2023-03-19 13:39:27 +0530

    refactor: clearer function name

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -2,13 +2,14 @@ function getReversedPhrase(phrase: string) {
   return phrase.split("").reverse().join("");
 }
 
-function getFormattedPhrase(phrase: string) {
-  return phrase.split(" ").join("").toLowerCase();
+function withoutSpaces(phrase: string) {
+  return phrase.split(" ").join("");
 }
 
 function palindrome(phrase: string) {
   return (
-    getFormattedPhrase(phrase) === getFormattedPhrase(getReversedPhrase(phrase))
+    withoutSpaces(phrase).toLowerCase() ===
+    withoutSpaces(getReversedPhrase(phrase)).toLowerCase()
   );
 }
 
