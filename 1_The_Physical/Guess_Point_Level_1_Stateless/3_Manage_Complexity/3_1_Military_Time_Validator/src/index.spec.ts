b33ac7245d6a2ecd94e2f0bc127ce9cec1401c5f
Rohan Gayen <rohangayen@gmail.com>
2023-03-21 01:30:04 +0530

    feat: '25:00 - 12:23' should not be valid range

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -4,4 +4,8 @@ describe("military time validator", () => {
   it("'01:12 - 14:32' should be valid range", () => {
     expect(isValidMilitaryTimeRange("01:12 - 14:32")).toBeTruthy();
   });
+
+  it("'25:00 - 12:23' should not be valid range", () => {
+    expect(isValidMilitaryTimeRange("25:00 - 12:23")).toBeFalsy();
+  });
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,5 +1,13 @@
+function isValidHour(number: number): boolean {
+  return number >= 0 && number <= 23;
+}
+
 function isValidMilitaryTimeRange(timeRange: string): boolean {
-  return true;
+  const [startHour, endHour] = timeRange
+    .split(" - ")
+    .map((time) => Number(time.split(":")[0]));
+
+  return isValidHour(startHour) && isValidHour(endHour);
 }
 
 export default isValidMilitaryTimeRange;
