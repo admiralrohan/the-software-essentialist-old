a667aaac93c158b88c9859b03302ae5841731577
Rohan Gayen <rohangayen@gmail.com>
2023-05-26 22:46:45 +0530

    red: knows that each return value is a string

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,7 @@
+import fizzbuzz from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-
-
+  it("knows that each return value is a string", () => {
+    expect(typeof fizzbuzz(1)).toBe("string");
+  });
 });
