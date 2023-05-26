16ca9348c5a0c2be9a2cf2b7e3546b0cd6452644
Rohan Gayen <rohangayen@gmail.com>
2023-05-26 22:54:03 +0530

    red: 5 returns 'Buzz'

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -8,4 +8,8 @@ describe("fizzbuzz", () => {
   it("knows that 3 returns 'Fizz' as this is multiple of 3", () => {
     expect(fizzbuzz(3)).toBe("Fizz");
   });
+
+  it("knows that 5 returns 'Buzz' as this is multiple of 3", () => {
+    expect(fizzbuzz(5)).toBe("Buzz");
+  });
 });
