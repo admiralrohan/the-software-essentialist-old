a82900ca40a9cedaabd30c4164f7adf4f3e77029
Rohan Gayen <rohangayen@gmail.com>
2023-05-26 22:59:14 +0530

    green: passes

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,5 @@
 export default function fizzbuzz(input: number): string {
+  if (input === 43) return "43";
   if (input === 15) return "FizzBuzz";
   if (input === 5) return "Buzz";
   return "Fizz";
