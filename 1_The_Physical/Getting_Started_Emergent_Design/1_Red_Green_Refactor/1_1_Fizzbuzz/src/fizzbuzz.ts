ed802ad61a77b7d1fe0b480fdbc28b0f379d6b24
Rohan Gayen <rohangayen@gmail.com>
2023-05-27 20:16:32 +0530

    green: pass

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,5 @@
 export default function fizzbuzz(input: number): string {
+  if (isNaN(input)) throw Error("Must be a number");
   if (input > 100 || input < 1) throw Error("Must be within 1 and 100");
   if (input === 43) return "43";
   if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz";
