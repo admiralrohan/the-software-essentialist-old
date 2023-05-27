b87873c16cbef8b699ecac60e369f32c81af7924
Rohan Gayen <rohangayen@gmail.com>
2023-05-28 00:19:01 +0530

    green: "Mom" returns true

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -4,4 +4,8 @@ describe("palindrome checker", () => {
   it('"mom" returns true', () => {
     expect(palindrome("mom")).toBeTruthy();
   });
+
+  it('"Mom" returns true', () => {
+    expect(palindrome("Mom")).toBeTruthy();
+  });
 });
