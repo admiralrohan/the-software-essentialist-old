939e387090d13a9a22d5116be55adcb77af3a4cb
Rohan Gayen <rohangayen@gmail.com>
2023-05-29 05:20:09 +0530

    feat: knows [2, 4, 21, -8, 53, 40] returns minimum value of -8
    Along with todos

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,14 @@
+import statsCalculator from ".";
 
-describe('stats calculator', () => {
+describe("stats calculator", () => {
+  it("knows [2, 4, 21, -8, 53, 40] returns minimum value of -8", () => {
+    const response = statsCalculator([2, 4, 21, -8, 53, 40]);
+    expect(response.min).toBe(-8);
+  });
 
-
-})
+  it.todo("knows [2, 4, 21, -8, 53, 40] returns maximum value of 53");
+  it.todo("knows [2, 4, 21, -8, 53, 40] returns no of elements of 6");
+  it.todo(
+    "knows [2, 4, 21, -8, 53, 40] returns average value of 18.666666666667"
+  );
+});
---
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
@@ -0,0 +1,15 @@
+interface IResponse {
+  min: number;
+  max: number;
+  noOfElements: number;
+  avg: number;
+}
+
+export default function statsCalculator(numbers: number[]): IResponse {
+  return {
+    min: numbers.reduce((min, curr) => (curr < min ? curr : min), 9999),
+    max: 0,
+    noOfElements: 0,
+    avg: 0,
+  };
+}
