961f09b41520453b6c3440c51c73dae0b3a1e7ab
Rohan Gayen <rohangayen@gmail.com>
2023-05-28 21:04:14 +0530

    feat: returns an invalid length error when strings like "passwordpassword" are more than 15 characters

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -12,4 +12,12 @@ describe("password validator", () => {
     expect(response.errors.length).toEqual(1);
     expect(response.errors[0]).toEqual("InvalidLengthError");
   });
+
+  it('returns an invalid length error when strings like "passwordpassword" are more than 15 characters', () => {
+    const response = checkPassword("passwordpassword");
+
+    expect(response.result).toBeFalsy();
+    expect(response.errors.length).toEqual(1);
+    expect(response.errors[0]).toEqual("InvalidLengthError");
+  });
 });
