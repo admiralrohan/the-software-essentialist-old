2ce7469186f8082a5c770435f36f0a1ab5c24993
Rohan Gayen <rohangayen@gmail.com>
2023-05-30 12:38:25 +0530

    refactor: encapsulate input array

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,23 +1,27 @@
 import statsCalculator from ".";
 
 describe("stats calculator", () => {
-  it("knows [2, 4, 21, -8, 53, 40] returns minimum value of -8", () => {
-    const response = statsCalculator([2, 4, 21, -8, 53, 40]);
+  const INPUT = [2, 4, 21, -8, 53, 40];
+
+  it(`knows [${INPUT.join(", ")}] returns minimum value of -8`, () => {
+    const response = statsCalculator(INPUT);
     expect(response.min).toBe(-8);
   });
 
-  it("knows [2, 4, 21, -8, 53, 40] returns maximum value of 53", () => {
-    const response = statsCalculator([2, 4, 21, -8, 53, 40]);
+  it(`knows [${INPUT.join(", ")}] returns maximum value of 53`, () => {
+    const response = statsCalculator(INPUT);
     expect(response.max).toBe(53);
   });
 
-  it("knows [2, 4, 21, -8, 53, 40] returns no of elements of 6", () => {
-    const response = statsCalculator([2, 4, 21, -8, 53, 40]);
+  it(`knows [${INPUT.join(", ")}] returns no of elements of 6`, () => {
+    const response = statsCalculator(INPUT);
     expect(response.noOfElements).toBe(6);
   });
 
-  it("knows [2, 4, 21, -8, 53, 40] returns average value of 18.666666666667", () => {
-    const response = statsCalculator([2, 4, 21, -8, 53, 40]);
+  it(`knows [${INPUT.join(
+    ", "
+  )}] returns average value of 18.666666666667`, () => {
+    const response = statsCalculator(INPUT);
     expect(response.avg).toBe(18.666666666667);
   });
 });
