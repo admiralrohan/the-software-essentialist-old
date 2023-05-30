9d51b3cb788a900653b1b07f57b180776550a657
Rohan Gayen <rohangayen@gmail.com>
2023-05-30 22:43:12 +0530

    feat: knows "25:00 - 12:23" is not valid time range

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -5,6 +5,8 @@ describe("military time validator", () => {
     expect(militaryTimeValidator("01:12 - 14:32")).toBeTruthy();
   });
 
-  it.todo(`knows "25:00 - 12:23" is not valid time range`);
+  it(`knows "25:00 - 12:23" is not valid time range`, () => {
+    expect(militaryTimeValidator("25:00 - 12:23")).toBeFalsy();
+  });
   it.todo(`knows "22:00 - 23:12" is valid time range`);
 });
--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,3 +1,17 @@
-export default function militaryTimeValidator(input: string): boolean {
-  return true;
+/**
+ * @param timeRange Example "25:00 - 12:23"
+ * @returns Whether it is valid time range
+ */
+export default function militaryTimeValidator(timeRange: string): boolean {
+  const splittedTimeRange = timeRange.split("-");
+  const startTime = splittedTimeRange[0];
+  const endTime = splittedTimeRange[1];
+
+  const isStartTimeValid =
+    Number(startTime.split(":")[0]) < 24 &&
+    Number(startTime.split(":")[1]) < 60;
+  const isEndTimeValid =
+    Number(endTime.split(":")[0]) < 24 && Number(endTime.split(":")[1]) < 60;
+
+  return isStartTimeValid && isEndTimeValid;
 }
