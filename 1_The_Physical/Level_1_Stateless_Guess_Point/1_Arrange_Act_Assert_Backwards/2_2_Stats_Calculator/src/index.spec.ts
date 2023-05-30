35ebc662b4e7696054230e018a27b76551778bdf
Rohan Gayen <rohangayen@gmail.com>
2023-05-30 12:46:28 +0530

    refactor: parametarization of min value test

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,27 +1,36 @@
 import statsCalculator from ".";
 
 describe("stats calculator", () => {
-  const INPUT = [2, 4, 21, -8, 53, 40];
+  const STANDARD_INPUT = [2, 4, 21, -8, 53, 40];
+  const INPUT_WITH_ZERO = [2, 4, 0, -6];
 
-  it(`knows [${INPUT.join(", ")}] returns minimum value of -8`, () => {
-    const response = statsCalculator(INPUT);
-    expect(response.min).toBe(-8);
+  describe("should return minimum value among passed numbers", () => {
+    it.each([
+      [STANDARD_INPUT, -8],
+      [INPUT_WITH_ZERO, -6],
+    ])(
+      "knows %p returns minimum value of %p",
+      (input: number[], result: number) => {
+        const response = statsCalculator(input);
+        expect(response.min).toBe(result);
+      }
+    );
   });
 
-  it(`knows [${INPUT.join(", ")}] returns maximum value of 53`, () => {
-    const response = statsCalculator(INPUT);
+  it(`knows [${STANDARD_INPUT.join(", ")}] returns maximum value of 53`, () => {
+    const response = statsCalculator(STANDARD_INPUT);
     expect(response.max).toBe(53);
   });
 
-  it(`knows [${INPUT.join(", ")}] returns no of elements of 6`, () => {
-    const response = statsCalculator(INPUT);
+  it(`knows [${STANDARD_INPUT.join(", ")}] returns no of elements of 6`, () => {
+    const response = statsCalculator(STANDARD_INPUT);
     expect(response.noOfElements).toBe(6);
   });
 
-  it(`knows [${INPUT.join(
+  it(`knows [${STANDARD_INPUT.join(
     ", "
   )}] returns average value of 18.666666666667`, () => {
-    const response = statsCalculator(INPUT);
+    const response = statsCalculator(STANDARD_INPUT);
     expect(response.avg).toBe(18.666666666667);
   });
 });
