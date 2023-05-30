6210c957c657e8d0dc8c11a9256ec0bec200f553
Rohan Gayen <rohangayen@gmail.com>
2023-05-30 22:43:55 +0530

    feat: knows "22:00 - 23:12" is valid time range

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -8,5 +8,7 @@ describe("military time validator", () => {
   it(`knows "25:00 - 12:23" is not valid time range`, () => {
     expect(militaryTimeValidator("25:00 - 12:23")).toBeFalsy();
   });
-  it.todo(`knows "22:00 - 23:12" is valid time range`);
+  it(`knows "22:00 - 23:12" is valid time range`, () => {
+    expect(militaryTimeValidator("22:00 - 23:12")).toBeTruthy();
+  });
 });
