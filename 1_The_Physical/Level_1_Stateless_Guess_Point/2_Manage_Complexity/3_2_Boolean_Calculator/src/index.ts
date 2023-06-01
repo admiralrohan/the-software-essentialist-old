0892f9462a03b0c79d76f84eb9958d41bcac6b4b
Rohan Gayen <rohangayen@gmail.com>
2023-06-01 10:48:51 +0530

    refactor: simplification of logic

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,11 +1,16 @@
+const converter: { [key: string]: boolean } = {
+  FALSE: false,
+  TRUE: true,
+};
+
 export default function booleanCalculator(expression: string): boolean {
   const words = expression.split(" ");
   console.log(words);
+
   if (words.length === 2) {
     if (words[0] === "NOT" && words[1] === "TRUE") return false;
     return true;
   }
 
-  if (expression === "FALSE") return false;
-  return true;
+  return converter[words[0]];
 }
