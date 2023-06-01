969d96f47a48bca7123632cabefc16ac9ead8b89
Rohan Gayen <rohangayen@gmail.com>
2023-06-01 09:29:19 +0530

    feat: list down test cases

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,4 +1,30 @@
+describe("boolean calculator", () => {
+  describe("should handle single values", () => {
+    it.todo(`knows "TRUE" returns true`);
+    it.todo(`knows "FALSE" returns false`);
+  });
 
-describe('boolean calculator', () => {
+  describe("should handle NOT operator", () => {
+    it.todo(`knows "NOT TRUE" returns false`);
+  });
 
-})
+  describe("should handle AND operator", () => {
+    it.todo(`knows "TRUE AND FALSE" returns false`);
+    it.todo(`knows "TRUE AND TRUE" returns true`);
+  });
+
+  describe("should handle OR operator", () => {
+    it.todo(`knows "TRUE OR FALSE" returns true`);
+    it.todo(`knows "FALSE OR FALSE" returns false`);
+  });
+
+  describe("should handle combination of operators w/ precedence", () => {
+    it.todo(`knows "TRUE OR TRUE OR TRUE AND FALSE" returns true`);
+    it.todo(`knows "TRUE OR FALSE AND NOT FALSE" returns false`);
+  });
+
+  describe("should handle parenthesis", () => {
+    it.todo(`knows "(TRUE OR TRUE OR TRUE) AND FALSE" returns false`);
+    it.todo(`knows "NOT (TRUE AND TRUE)" returns false`);
+  });
+});
