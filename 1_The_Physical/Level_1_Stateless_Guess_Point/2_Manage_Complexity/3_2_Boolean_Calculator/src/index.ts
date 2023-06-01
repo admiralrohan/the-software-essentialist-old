d56d2d799ef1cc2a45cecd7e97841f1f41af3672
Rohan Gayen <rohangayen@gmail.com>
2023-06-01 12:45:25 +0530

    refactor: remove code from "fake it" strategy early on

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -11,53 +11,41 @@ export default function booleanCalculator(expression: string): boolean {
   let wordsLength = words.length;
   // console.log(words);
 
-  if (words.length > 3) {
-    for (let i = 0; i < wordsLength; ) {
-      if (words[i] === "NOT") {
-        const result = words[i + 1] === "TRUE" ? "FALSE" : "TRUE";
-        words[i] = result;
-        words = words.filter((_, index) => index !== i + 1);
-        wordsLength = words.length;
-      } else {
-        i++;
-      }
+  for (let i = 0; i < wordsLength; ) {
+    if (words[i] === "NOT") {
+      const result = words[i + 1] === "TRUE" ? "FALSE" : "TRUE";
+      words[i] = result;
+      words = words.filter((_, index) => index !== i + 1);
+      wordsLength = words.length;
+    } else {
+      i++;
     }
-    // console.log(words);
+  }
+  // console.log(words);
 
-    for (let i = 0; i < words.length; ) {
-      if (words[i] === "AND") {
-        const result = toBool[words[i - 1]] && toBool[words[i + 1]];
-        words[i] = result === true ? "TRUE" : "FALSE";
-        words = words.filter((_, index) => index !== i - 1 && index !== i + 1);
-        wordsLength = words.length;
-      } else {
-        i++;
-      }
+  for (let i = 0; i < words.length; ) {
+    if (words[i] === "AND") {
+      const result = toBool[words[i - 1]] && toBool[words[i + 1]];
+      words[i] = result === true ? "TRUE" : "FALSE";
+      words = words.filter((_, index) => index !== i - 1 && index !== i + 1);
+      wordsLength = words.length;
+    } else {
+      i++;
     }
-    // console.log(words);
+  }
+  // console.log(words);
 
-    for (let i = 0; i < words.length; ) {
-      if (words[i] === "OR") {
-        const result = toBool[words[i - 1]] || toBool[words[i + 1]];
-        words[i] = result === true ? "TRUE" : "FALSE";
-        words = words.filter((_, index) => index !== i - 1 && index !== i + 1);
-        wordsLength = words.length;
-      } else {
-        i++;
-      }
+  for (let i = 0; i < words.length; ) {
+    if (words[i] === "OR") {
+      const result = toBool[words[i - 1]] || toBool[words[i + 1]];
+      words[i] = result === true ? "TRUE" : "FALSE";
+      words = words.filter((_, index) => index !== i - 1 && index !== i + 1);
+      wordsLength = words.length;
+    } else {
+      i++;
     }
-    // console.log(words);
   }
-
-  if (words.length === 3) {
-    if (words[1] === "AND") return toBool[words[0]] && toBool[words[2]];
-    return toBool[words[0]] || toBool[words[2]];
-  }
-
-  if (words.length === 2) {
-    if (words[0] === "NOT" && words[1] === "TRUE") return false;
-    return true;
-  }
+  // console.log(words);
 
   return toBool[words[0]];
 }
